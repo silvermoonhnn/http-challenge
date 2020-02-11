c44122c95f174a41ba9f623a7793d9f2e6d3ce97
Gigaming <gigaming@Gigamings-iMac-2.local>
2020-02-11 10:56:51 +0700

    http challenge

---
+++ httpChallenge/httpChallenge/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace httpChallenge
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Avatar_url { get; set; }
+        public string Employee_id { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public DateTime Birthday { get; set; }
+        public List<Address> Addresses { get; set; }
+        public List<Phone> Phones { get; set; }
+        public List<DateTime> Presence_list { get; set; }
+        public int Salary { get; set; }
+        public Departement Department { get; set; }
+        public Position Position { get; set; }
+    }
+
+    public class Address
+    {
+        public string Label { get; set; }
+        public string Addresses { get; set; }
+        public string City { get; set; }
+    }
+
+    public class Phone
+    {
+        public string Label { get; set; }
+        public string Phones { get; set; }
+    }
+
+    public class Departement
+    {
+        public string Name { get; set; }
+    }
+
+    public class Position
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ httpChallenge/httpChallenge/Scraping.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using IronWebScraper;
+
+namespace httpChallenge
+{
+    public class Scraping : WebScraper
+    {
+        public override void Init()
+        {
+            this.LoggingLevel = WebScraper.LogLevel.All;
+            this.Request("https://www.kompas.com/", Parse);
+        }
+
+        public override void Parse(Response response)
+        {
+            foreach (var css1 in response.Css("latest ga--latest mt2 clearfix"))
+            {
+                string title = css1.TextContentClean;
+            }
+        }
+    }
+}
